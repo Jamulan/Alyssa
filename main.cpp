fd174d84bdb8ee81eb72c483eeb03f0c7f8c9b51
jamulan <jamulan@ijuggle5.com>
2020-11-08 12:15:10 -0600

    added shader modules and started to construct the graphics pipeline

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <set>
 #include <cstdint>
 #include <algorithm>
+#include <fstream>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -24,7 +25,7 @@ const std::vector<const char*> deviceExtensions = {
 };
 
 #ifdef NDEBUG
-const bool enableValidationLayers = false;
+    const bool enableValidationLayers = false;
 #else
     const bool enableValidationLayers = true;
 #endif
@@ -43,6 +44,24 @@ struct SwapChainSupportDetails {
     std::vector<VkSurfaceFormatKHR> formats;
     std::vector<VkPresentModeKHR> presentModes;
 };
+
+static std::vector<char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if(!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
+
+    size_t fileSize = (size_t) file.tellg();
+    std::vector<char> buffer(fileSize);
+
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+
+    file.close();
+
+    return buffer;
+}
 
 class HelloTriangleApplication {
 public:
@@ -82,6 +101,7 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        createGraphicsPipeline();
     }
 
     void createInstance() {
@@ -441,6 +461,43 @@ private:
             if(vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
                 throw std::runtime_error("failed to create image views!");
             }
+        }
+    }
+
+    void createGraphicsPipeline() {
+        auto vertShaderCode = readFile("shaders/vert.spv");
+        auto fragShaderCode = readFile("shaders/frag.spv");
+
+        VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+        VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+        VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+        vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vertShaderStageInfo.module = vertShaderModule;
+        vertShaderStageInfo.pName = "main";
+
+        VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+        fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        fragShaderStageInfo.module = fragShaderModule;
+        fragShaderStageInfo.pName = "main";
+
+        VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+        vkDestroyShaderModule(device, vertShaderModule, nullptr);
+        vkDestroyShaderModule(device, fragShaderModule, nullptr);
+    }
+
+    VkShaderModule createShaderModule(const std::vector<char>& code) {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+        VkShaderModule shaderModule;
+        if(vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create shader module!");
         }
     }
 
--- makefile
+++ makefile
@@ -1,15 +1,14 @@
-VULKAN_SDK_PATH = /home/jamulan/Dev/Vulkan/1.2.148.1/x86_64
+VULKAN_SDK_PATH = /home/jamulan/Dev/VulkanSDK/1.2.148.1/x86_64
 
 CFLAGS = -std=c++17 -I$(VULKAN_SDK_PATH)/include
 LDFLAGS = -L$(VULKAN_SDK_PATH)/lib `pkg-config --static --libs glfw3` -lvulkan
 
 all: Vulkan_First Vulkan_First-Debug
 
-
-Vulkan_First-Debug: main.cpp
+Vulkan_First-Debug: main.cpp Shaders
 	g++ $(CFLAGS) -g -o make-build/Vulkan_First-Debug main.cpp $(LDFLAGS)
 
-Vulkan_First: main.cpp
+Vulkan_First: main.cpp Shaders
 	g++ $(CFLAGS) -o make-build/Vulkan_First main.cpp $(LDFLAGS)
 
 .PHONY: test clean
@@ -19,3 +18,9 @@ test: Vulkan_First
 
 clean:
 	rm -rf make-build/Vulkan_First
+	rm -rf make-build/shaders/*.spv
+
+Shaders: shaders/shader.vert shaders/shader.frag
+	mkdir -p make-build/shaders
+	$(VULKAN_SDK_PATH)/bin/glslc shaders/shader.vert -o make-build/shaders/vert.spv
+	$(VULKAN_SDK_PATH)/bin/glslc shaders/shader.frag -o make-build/shaders/frag.spv
