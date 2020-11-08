cc55cba7b524fd9a0303c46cbb06128070b31219
jamulan <jamulan@ijuggle5.com>
2020-11-08 10:07:54 -0600

    fixed typo

--- main.cpp
+++ main.cpp
@@ -427,10 +427,10 @@ private:
             createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
             createInfo.format = swapChainImageFormat;
 
-            createInfo.components.r = VK_COMPONENT_SWIZLE_IDENTITY;
-            createInfo.components.g = VK_COMPONENT_SWIZLE_IDENTITY;
-            createInfo.components.b = VK_COMPONENT_SWIZLE_IDENTITY;
-            createInfo.components.a = VK_COMPONENT_SWIZLE_IDENTITY;
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
 
             createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
             createInfo.subresourceRange.baseMipLevel = 0;
