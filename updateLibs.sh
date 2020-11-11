1e1211c1cb303fa674e8a25e3dc79ba51b617d9a
jamulan <jamulan@ijuggle5.com>
2020-11-10 19:58:07 -0600

    please ignore my stupidity

--- updateLibs.sh
+++ updateLibs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p libs
-curl "https://raw.githubusercontent.com/nothings/stb/master/stb_image.h" > stb_image.h
-curl "https://raw.githubusercontent.com/tinyobjloader/tinyobjloader/master/tiny_obj_loader.h" > tiny_obj_loader.h
+curl "https://raw.githubusercontent.com/nothings/stb/master/stb_image.h" > libs/stb_image.h
+curl "https://raw.githubusercontent.com/tinyobjloader/tinyobjloader/master/tiny_obj_loader.h" > libs/tiny_obj_loader.h
